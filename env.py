fd672bcf629e099dd33ecc9edd96cb9738e325c6
juhonamnam <namjhnam@gmail.com>
2022-06-01 01:38:26 +0900

    modify env var

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ import os
 api_key = os.getenv('CATS_TELE_KEY')
 admin_id = os.getenv('CATS_INITIAL_ADMIN_ID')
 admin_name = os.getenv('CATS_INITIAL_ADMIN_NAME', 'First Admin')
-sql_url = os.getenv('SQL_URL', 'sqlite:///test.db?check_same_thread=False')
+sql_url = os.getenv('CATS_SQL_URL', 'sqlite:///test.db?check_same_thread=False')

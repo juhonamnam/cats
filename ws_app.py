12e9abf3272640fb64b2069e213b130e67be5316
juhonamnam <jhnamnam@gmail.com>
2022-06-29 17:00:36 +0900

    modify logger

--- ws_app.py
+++ ws_app.py
@@ -4,8 +4,11 @@ from src.websocket.upbit_websocket import UpbitWebsocket
 from src.main.controller.base import controller
 import logging.config
 import json
+import os
 
 if is_production:
+    if not os.path.exists('./logs'):
+        os.mkdir('./logs')
     logging.config.dictConfig(json.load(open('./logger.ws.json')))
 else:
     logging.config.dictConfig(json.load(open('./logger.dev.json')))

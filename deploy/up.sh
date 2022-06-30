e63315e86007e897363ff9f4ec25e3c1fe7b3d50
juhonamnam <jhnamnam@gmail.com>
2022-06-30 16:03:29 +0900

    modify deploy script

--- deploy/up.sh
+++ deploy/up.sh
@@ -1,8 +1,22 @@
+# version
+. ./deploy/version.sh
+
 # down
 . ./deploy/down.sh
 
 # up
 mkdir -p logs
-docker build -f Dockerfile.production --tag $APP_NAME:$APP_VERSION .
-docker run --name $APP_NAME-main -v $(pwd)/logs:/app/logs -d $APP_NAME:$APP_VERSION main_app.py production
-docker run --name $APP_NAME-ws -v $(pwd)/logs:/app/logs -d $APP_NAME:$APP_VERSION ws_app.py production
+
+docker build \
+    -f Dockerfile.production \
+    --tag $APP_NAME:$APP_VERSION .
+
+docker run \
+    --name $APP_NAME-main \
+    -v $(pwd)/logs:/app/logs \
+    -d $APP_NAME:$APP_VERSION main_app.py production
+
+docker run \
+    --name $APP_NAME-ws \
+    -v $(pwd)/logs:/app/logs \
+    -d $APP_NAME:$APP_VERSION ws_app.py production

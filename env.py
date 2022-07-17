0fb5f0aabd8f741fb2f8058668d231eca7c159cc
juhonamnam <jhnamnam@gmail.com>
2022-07-17 18:39:07 +0900

    delete db initialize

--- env.py
+++ env.py
@@ -2,7 +2,5 @@ import sys
 import os
 
 api_key = os.getenv('TELE_KEY')
-admin_id = os.getenv('INITIAL_ADMIN_ID')
-admin_name = os.getenv('INITIAL_ADMIN_NAME', 'First Admin')
 sql_url = os.getenv('SQL_URL')
 is_production = len(sys.argv) > 1 and sys.argv[1] == 'production'
